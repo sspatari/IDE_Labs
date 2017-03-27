c3586005ad3d3dac2c528347c36204e1761515ac
alexeisersun <alex.sersun@yahoo.com>
2017-03-27 13:23:05 +0300

    Updated event-handlers.

--- Lab2Calculator/MainWindow.xaml.cs
+++ Lab2Calculator/MainWindow.xaml.cs
@@ -1,17 +1,4 @@
-﻿using System;
-using System.Collections.Generic;
-using System.Linq;
-using System.Text;
-using System.Threading.Tasks;
-using System.Windows;
-using System.Windows.Controls;
-using System.Windows.Data;
-using System.Windows.Documents;
-using System.Windows.Input;
-using System.Windows.Media;
-using System.Windows.Media.Imaging;
-using System.Windows.Navigation;
-using System.Windows.Shapes;
+﻿using System.Windows;
 
 namespace Lab2Calculator
 {
@@ -20,92 +7,111 @@ namespace Lab2Calculator
     /// </summary>
     public partial class MainWindow : Window
     {
+        ICalculator calculator;
+
         public MainWindow()
         {
             InitializeComponent();
+            
         }
 
         private void clearAllButton_Click(object sender, RoutedEventArgs e)
         {
-
+            calculator.onCancelButton_Click("");
         }
 
         private void inversionButton_Click(object sender, RoutedEventArgs e)
         {
-
+            calculator.onInversionButton_Click("");
         }
 
         private void sqrtButton_Click(object sender, RoutedEventArgs e)
         {
-
-        }
-        private void dotButton_Click(object sender, RoutedEventArgs e)
-        {
-
+            calculator.onSqrtButton_Click("");
         }
 
         private void resultButton_Click(object sender, RoutedEventArgs e)
         {
-
+            calculator.onResultButton_Click("");
         }
 
-        private void additionButton_Click(object sender, RoutedEventArgs e)
+        private void dotButton_Click(object sender, RoutedEventArgs e)
         {
-
+            calculator.onRegularButton_Click(".");
         }
         
+        private void additionButton_Click(object sender, RoutedEventArgs e)
+        {
+            calculator.onRegularButton_Click("+");
+        }
+
         private void multiplyButton_Click(object sender, RoutedEventArgs e)
         {
+            calculator.onRegularButton_Click("*");
+        }
 
+        private void divisionButton_Click(object sender, RoutedEventArgs e)
+        {
+            calculator.onRegularButton_Click("/");
+        }
+
+        private void powerButton_Click(object sender, RoutedEventArgs e)
+        {
+            calculator.onRegularButton_Click("^");
         }
 
         private void digit0Button_Click(object sender, RoutedEventArgs e)
         {
+            calculator.onRegularButton_Click("0");
 
         }
-        
+
         private void digit1Button_Click(object sender, RoutedEventArgs e)
         {
+            calculator.onRegularButton_Click("1");
 
         }
 
         private void digit2Button_Click(object sender, RoutedEventArgs e)
         {
+            calculator.onRegularButton_Click("2");
 
         }
 
         private void digit3Button_Click(object sender, RoutedEventArgs e)
         {
-
+            calculator.onRegularButton_Click("3");
         }
 
         private void digit4Button_Click(object sender, RoutedEventArgs e)
         {
-
+            calculator.onRegularButton_Click("4");
         }
 
         private void digit5Button_Click(object sender, RoutedEventArgs e)
         {
-
+            calculator.onRegularButton_Click("5");
         }
 
         private void digit6Button_Click(object sender, RoutedEventArgs e)
         {
-
+            calculator.onRegularButton_Click("6");
         }
-        
+
         private void digit7Button_Click(object sender, RoutedEventArgs e)
         {
+            calculator.onRegularButton_Click("7");
 
         }
 
         private void digit8Button_Click(object sender, RoutedEventArgs e)
         {
-
+            calculator.onRegularButton_Click("8");
         }
 
         private void digit9Button_Click(object sender, RoutedEventArgs e)
         {
+            calculator.onRegularButton_Click("9");
 
         }
     }
