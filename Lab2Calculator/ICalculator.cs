f2711dcf75a38c90515d6ef4ae56eefae374adb3
sspatari <spatari.stanislav@gmail.com>
2017-03-28 19:11:22 +0500

    Modified Calculator interface

--- Lab2Calculator/ICalculator.cs
+++ Lab2Calculator/ICalculator.cs
@@ -1,5 +1,6 @@
 ﻿using System;
 using System.Collections.Generic;
+using System.ComponentModel;
 using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
@@ -7,28 +8,14 @@ using System.Windows;
 
 namespace Lab2Calculator
 {
-    interface ICalculator
+    interface ICalculator:INotifyPropertyChanged
     {
         String screenText { get; set; }
-        void onDigit0Button_Click(object sender, RoutedEventArgs e);
-        void onDigit1Button_Click(object sender, RoutedEventArgs e);
-        void onDigit2Button_Click(object sender, RoutedEventArgs e);
-        void onDigit3Button_Click(object sender, RoutedEventArgs e);
-        void onDigit4Button_Click(object sender, RoutedEventArgs e);
-        void onDigit5Button_Click(object sender, RoutedEventArgs e);
-        void onDigit6Button_Click(object sender, RoutedEventArgs e);
-        void onDigit7Button_Click(object sender, RoutedEventArgs e);
-        void onDigit8Button_Click(object sender, RoutedEventArgs e);
-        void onDigit9Button_Click(object sender, RoutedEventArgs e);
-        void onPointButton_Click(object sender, RoutedEventArgs e);
-        void onEqualSignButton_Click(object sender, RoutedEventArgs e);
-        void onPlusButton_Click(object sender, RoutedEventArgs e);
-        void onMinusButton_Click(object sender, RoutedEventArgs e);
-        void onDivButton_Click(object sender, RoutedEventArgs e);
-        void onMultiplicationButton_Click(object sender, RoutedEventArgs e);
-        void onCancelButton_Click(object sender, RoutedEventArgs e);
-        void onInversionButton_Click(object sender, RoutedEventArgs e);
-        void onSqrtButton_Click(object sender, RoutedEventArgs e);
+        void onRegularButton_Click(String text);
+        void onCancelButton_Click(String text);
+        void onInversionButton_Click(String text);
+        void onSqrtButton_Click(String text);
+        void onResultButton_Click(String text);
     }
 
 }
