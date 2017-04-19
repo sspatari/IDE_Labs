49455e1168b81e29d1b0401ef257ab71e50fc38a
alexeisersun <alex.sersun@yahoo.com>
2017-04-19 20:14:00 +0300

    Added another UI layout. Moved Main Window's XAML into a separate User Control.

---
+++ Lab2Calculator/CalculatorClass.cs
@@ -0,0 +1,56 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Lab2Calculator
+{
+    class CalculatorClass : ICalculator
+    {
+        private string _text;
+
+        public event PropertyChangedEventHandler PropertyChanged = delegate { };
+
+        public string screenText
+        {
+            get
+            {
+                return _text;
+            }
+
+            set
+            {
+                _text = value;
+                PropertyChanged(this, new PropertyChangedEventArgs("screenText"));
+            }
+        }
+
+        public void onCancelButton_Click()
+        {
+            
+        }
+
+        public void onInversionButton_Click()
+        {
+           
+        }
+
+        public String onRegularButton_Click(String character)
+        {
+            screenText = screenText + character;
+            return screenText;
+        }
+
+        public void onResultButton_Click()
+        {
+            
+        }
+
+        public void onSqrtButton_Click()
+        {
+            
+        }
+    }
+}
---
+++ Lab2Calculator/StandartUserControl.xaml.cs
@@ -0,0 +1,125 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows;
+using System.Windows.Controls;
+using System.Windows.Data;
+using System.Windows.Documents;
+using System.Windows.Input;
+using System.Windows.Media;
+using System.Windows.Media.Imaging;
+using System.Windows.Navigation;
+using System.Windows.Shapes;
+
+namespace Lab2Calculator
+{
+    /// <summary>
+    /// Interaction logic for StandartUserControl.xaml
+    /// </summary>
+    public partial class StandartUserControl : UserControl
+    {
+        public StandartUserControl()
+        {
+            InitializeComponent();
+        }
+
+        private void clearAllButton_Click(object sender, RoutedEventArgs e)
+        {
+            Console.WriteLine(e.ToString());
+        }
+
+        private void inversionButton_Click(object sender, RoutedEventArgs e)
+        {
+
+        }
+
+        private void sqrtButton_Click(object sender, RoutedEventArgs e)
+        {
+
+        }
+
+        private void resultButton_Click(object sender, RoutedEventArgs e)
+        {
+
+        }
+
+        private void additionButton_Click(object sender, RoutedEventArgs e)
+        {
+
+        }
+
+        private void multiplyButton_Click(object sender, RoutedEventArgs e)
+        {
+
+        }
+
+        private void powerButton_Click(object sender, RoutedEventArgs e)
+        {
+
+        }
+
+        private void digit0Button_Click(object sender, RoutedEventArgs e)
+        {
+
+        }
+
+        private void digit1Button_Click(object sender, RoutedEventArgs e)
+        {
+
+        }
+
+        private void digit2Button_Click(object sender, RoutedEventArgs e)
+        {
+
+        }
+
+        private void digit3Button_Click(object sender, RoutedEventArgs e)
+        {
+
+        }
+
+        private void digit4Button_Click(object sender, RoutedEventArgs e)
+        {
+
+        }
+
+        private void digit5Button_Click(object sender, RoutedEventArgs e)
+        {
+
+        }
+
+        private void digit6Button_Click(object sender, RoutedEventArgs e)
+        {
+
+        }
+
+        private void digit7Button_Click(object sender, RoutedEventArgs e)
+        {
+
+        }
+
+        private void digit8Button_Click(object sender, RoutedEventArgs e)
+        {
+
+        }
+
+        private void digit9Button_Click(object sender, RoutedEventArgs e)
+        {
+
+        }
+
+        private void divisionButton_Click(object sender, RoutedEventArgs e)
+        {
+
+        }
+
+        private void dotButton_Click(object sender, RoutedEventArgs e)
+        {
+
+        }
+
+
+    }
+}
