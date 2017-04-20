8d61df4f79061d6fcbf9cb88e683f93cc6259984
alexeisersun <alex.sersun@yahoo.com>
2017-04-20 07:18:29 +0300

    Populated OnButtonPress handler with corresponding handlers for different original sources.

--- Lab2Calculator/MainWindow.xaml.cs
+++ Lab2Calculator/MainWindow.xaml.cs
@@ -42,6 +42,8 @@ namespace Lab2Calculator
             }
         }
 
+        ICalculator calculator;
+
         public MainWindow()
         {
             InitializeComponent();
@@ -52,12 +54,71 @@ namespace Lab2Calculator
 
         public void OnPressButton(object sender, RoutedEventArgs e)
         {
-            screenText += "a";
             Console.WriteLine((e.OriginalSource as FrameworkElement).Name);
             
-            //TODO: list below all button names and the corresponding handlers, if any
             switch ((e.OriginalSource as FrameworkElement).Name)
             {
+                case "Num0":
+                    calculator?.onRegularButton_Click("0");
+                    screenText += "a";
+                    break;
+                case "Num1":
+                    calculator?.onRegularButton_Click("1");
+                    break;
+                case "Num2":
+                    calculator?.onRegularButton_Click("2");
+                    break;
+                case "Num3":
+                    calculator?.onRegularButton_Click("3");
+                    break;
+                case "Num4":
+                    calculator?.onRegularButton_Click("4");
+                    break;
+                case "Num5":
+                    calculator?.onRegularButton_Click("5");
+                    break;
+                case "Num6":
+                    calculator?.onRegularButton_Click("6");
+                    break;
+                case "Num7":
+                    calculator?.onRegularButton_Click("7");
+                    break;
+                case "Num8":
+                    calculator?.onRegularButton_Click("8");
+                    break;
+                case "Num9":
+                    calculator?.onRegularButton_Click("9");
+                    break;
+                case "ChangeUI":
+                    
+                    break;
+                case "Power":
+                    calculator?.onRegularButton_Click("^");
+                    break;
+                case "Dot":
+                    calculator?.onRegularButton_Click(".");
+                    break;
+                case "Add":
+                    calculator?.onRegularButton_Click("+");
+                    break;
+                case "Divide":
+                    calculator?.onRegularButton_Click("/");
+                    break;
+                case "Multiply":
+                    calculator?.onRegularButton_Click("*");
+                    break;
+                case "Subtract":
+                    calculator?.onRegularButton_Click("-");
+                    break;
+                case "Sqrt":
+                    calculator?.onSqrtButton_Click("");
+                    break;
+                case "Inverse":
+                    calculator?.onInversionButton_Click("");
+                    break;
+                case "ClearAll":
+                    calculator?.onCancelButton_Click("");
+                    break;
                 default:
                     break;
             };
