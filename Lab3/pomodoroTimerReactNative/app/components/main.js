cdf4adced974d1cdf7c03324b1f342a589f75c00
sspatari <spatari.stanislav@gmail.com>
2017-05-23 18:40:42 +0300

    simple android layout with the Pomodoro Timer Title

---
+++ Lab3/pomodoroTimerReactNative/app/components/main.js
@@ -0,0 +1,30 @@
+import React, { Component } from 'react';
+import { AppRegistry, StyleSheet, Text, View } from 'react-native';
+
+export default class Main extends Component {
+  render() {
+    return (
+      <View style={styles.container}>
+        <Text style={styles.welcome}>Pomodoro Timer</Text>
+      </View>
+    );
+  }
+}
+
+const styles = StyleSheet.create({
+  container: {
+    display: 'flex',
+    justifyContent: 'center',
+    alignItems: 'center',
+    backgroundColor: 'tomato',
+    marginTop: 60
+  },
+  welcome: {
+    color: 'white',
+    fontSize: 30,
+    textAlign: 'center',
+    margin: 50
+  }
+});
+
+AppRegistry.registerComponent('Main', () => Main)
